d351d0d5393df0ecf113cd3642cfc68f47c1bfeb
Айдин Бектенов <a.bektenov@gettransfer.com>
2018-07-17 15:57:00 +0300

    install framework

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,48 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/changelog">Change Log</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  export default {
+    beforeCreate () {
+      this.$Loading.start()
+    },
+    created () {
+      this.$Loading.finish()
     }
   }
-}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    h1 {
+      margin: 0 0 20px;
+      text-align: center;
+    }
+  }
+  #nav {
+    text-align: center;
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+  .at-loading-bar.at-loading-bar--success {
+    .at-loading-bar__inner {
+      background-color: #42b983;
+    }
+  }
 </style>
---
+++ src/views/ChangeLogPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>Change log</h1>
+    <div class="container">
+      <at-timeline pending>
+        <at-timeline-item color="red">
+          <i slot="dot" class="icon icon-package"></i>
+          <p>Install framework</p>
+          <small>17.07.18</small>
+        </at-timeline-item>
+        <at-timeline-item color="green">
+          <i slot="dot" class="icon icon-settings"></i>
+          <p>Vue init</p>
+          <small>17.07.18</small>
+        </at-timeline-item>
+      </at-timeline>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .at-timeline {
+    .at-timeline__content {
+      top: -3px;
+      padding: 0 0 20px 24px;
+    }
+  }
+</style>
